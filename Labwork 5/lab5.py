5a52ecaa29d084d553ec48c1c1a571fd9dce6c36
NhacBatQuan <thong1234das@yahoo.com.vn>
2022-12-25 14:40:59 +0700

    update labwork 5 gpu without shared memory

--- Labwork 5/lab5.py
+++ Labwork 5/lab5.py
@@ -8,7 +8,16 @@ from numba import cuda
 
 NUMBA_CUDA_DRIVER="/usr/lib/wsl/lib/libcuda.so.1"
 LD_LIBRARY_PATH="/usr/lib/wsl/lib/"
+
 image = plt.imread("/home/mocavandao/Lab/lechonk.jpg")
+
+height, width = image.shape[0], image.shape[1]
+pixelCount = width * height
+blockSize = (2, 2)
+gridSize = (math.ceil(height/blockSize[0]), math.ceil(width/blockSize[1]))
+reshaped_img = np.reshape(image, (height * width, 3))
+devOutput = cuda.device_array((height,width,3), np.uint8)
+devInput = cuda.to_device(image)
 
 filter = np.array([[0,  0,   1,  2,  1,  0,  0],
        [0,  3,  13, 22, 13,  3,  0],
@@ -19,23 +28,53 @@ filter = np.array([[0,  0,   1,  2,  1,  0,  0],
        [0,  0,   1,  2,  1,  0,  0]
        ])
 
-filter = filter/ np.sum(filter)
+filter2 = filter/ 1003
+devFilter = cuda.to_device(filter2)
 
 blur = image.copy()
 def conv(image,image2):
-    for x in range(3, image.shape[0]-3):
-        for y in range(3, image.shape[1]-3):
+    for x in range(3, height - 3):
+        for y in range(3, width - 3):
             r = 0
             g = 0
             b = 0
-            for i in range(-3, 3):
-                for j in range(-3,3):
-                    r = (r + image[x+i,y+j,0] * filter[i, j])
-                    g = (g + image[x+i,y+j,1] * filter[i, j])
-                    b = (b + image[x+i,y+j,2] * filter[i, j])
-            image2[x,y,0] = r
-            image2[x,y,1] = g
-            image2[x,y,2] = b
+            for i in range(-3, 4):
+                for j in range(-3,4):
+                    r = (r + image[x+i,y+j,0] * filter2[i, j])
+                    g = (g + image[x+i,y+j,1] * filter2[i, j])
+                    b = (b + image[x+i,y+j,2] * filter2[i, j])
+            image2[x, y, 0] = r
+            image2[x, y, 1] = g
+            image2[x, y, 2] = b
 
 conv(image,blur)
 plt.imsave('blur.png',blur)
+
+@cuda.jit
+def blur(src, dst, fil):
+    tidx = cuda.threadIdx.x + cuda.blockIdx.x * cuda.blockDim.x
+    tidy = cuda.threadIdx.y + cuda.blockIdx.y * cuda.blockDim.y
+    r = 0
+    g = 0
+    b = 0
+    for i in (range(-3,4)):
+        for j in (range(-3,4)):
+            r= np.uint8(r + (src[tidx+i, tidy+j, 0]*fil[i, j]))
+            g= np.uint8(g + (src[tidx+i, tidy+j, 1]*fil[i, j]))
+            b= np.uint8(b + (src[tidx+i, tidy+j, 2]*fil[i, j]))
+  
+    dst[tidx, tidy, 0 ] = r
+    dst[tidx, tidy, 1 ] = g
+    dst[tidx, tidy, 2 ] = b
+
+blur[gridSize, blockSize](devInput, devOutput,devFilter)
+hostOutput = devOutput.copy_to_host()
+
+plt.imsave('blur_gpu.png',hostOutput)
+
+
+
+
+
+
+
