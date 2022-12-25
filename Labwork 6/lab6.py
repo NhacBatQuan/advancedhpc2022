15578fc2dff1927bf01f273ad8812224b76e8afa
NhacBatQuan <thong1234das@yahoo.com.vn>
2022-12-25 16:08:44 +0700

    update labwork 6 brightness

--- Labwork 6/lab6.py
+++ Labwork 6/lab6.py
@@ -16,7 +16,7 @@ pixelCount = width * height
 blockSize = (2, 2)
 gridSize = (math.ceil(height/blockSize[0]), math.ceil(width/blockSize[1]))
 devOutput = cuda.device_array((height,width,3), np.uint8)
-
+devOutput1 = cuda.device_array((height,width,3), np.uint8)
 devInput = cuda.to_device(image)
 
 @cuda.jit
@@ -31,10 +31,27 @@ def biGray(src,dst):
     else:
         f = 0
     dst[tidx,tidy] = f
-    
+
 start_bigray = time.time()
 biGray[gridSize, blockSize](devInput, devOutput)
 print("GPU binary gray time :",abs(time.time()-start_bigray))
 hostOutput = devOutput.copy_to_host()
 
 plt.imsave("Binarization gray.png",hostOutput, cmap = 'gray')
+
+@cuda.jit
+def bright(src,dst, brightness):
+    tidx = cuda.threadIdx.x + cuda.blockIdx.x * cuda.blockDim.x
+    tidy = cuda.threadIdx.y + cuda.blockIdx.y * cuda.blockDim.y
+
+    fr = np.uint8(src[tidx,tidy,0] * brightness)
+    fg = np.uint8(src[tidx,tidy,1] * brightness)
+    fb = np.uint8(src[tidx,tidy,2] * brightness)
+
+    dst[tidx,tidy,0] = fr
+    dst[tidx,tidy,1] = fg
+    dst[tidx,tidy,2] = fb
+
+bright[gridSize, blockSize](devInput, devOutput1, 1.1)
+hostOutput1 = devOutput1.copy_to_host()
+plt.imsave("brightness.png",hostOutput1)
